c2b3801a6f6ebdadac4af4442cdaf547110384d0
ishidukaryo <hekkusyun1@gmail.com>
2020-12-04 15:05:25 +0900

    PHPchapter2課題

--- chap1_2.php
+++ chap1_2.php
@@ -1,8 +1,8 @@
 <?php
 var_dump("上底は？");
-$top = fgets( STDIN );
+$top = fgets(STDIN);
 var_dump("下底は？");
-$bottom = fgets( STDIN );
+$bottom = fgets(STDIN);
 var_dump("高さは？");
-$height = fgets( STDIN );
-var_dump("面積は".($top+$bottom)*$height/2);
+$height = fgets (STDIN);
+var_dump("面積は" . ( $top + $bottom ) * $height / 2);
