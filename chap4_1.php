2a87176e032646cc27de7c7afb2fc88247bd6247
ishidukaryo <hekkusyun1@gmail.com>
2020-12-09 21:30:06 +0900

    chapter4課題

--- chap4_1.php
+++ chap4_1.php
@@ -1,4 +1,14 @@
 <?php
-require_once __DIR__ . "chap4_area.php";
-calcArea(10, 20, 30);
+require_once __DIR__ . "/chap4_area.php";
+
+$top = 10;
+$bottom = 20;
+$height = 30;
+$area = calcArea($top, $bottom, $height);
+displayMsg($top, $bottom, $height, $area);
+
+$top = 40;
+$bottom = 50;
+$height = 60;
+$area = calcArea($top, $bottom, $height);
 displayMsg($top, $bottom, $height, $area);
