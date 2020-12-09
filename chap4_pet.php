4ef2e4e2f776bc3f4bc7c2b9cc23dc370916f92c
ishidukaryo <hekkusyun1@gmail.com>
2020-12-10 03:15:34 +0900

    chapter4課題

--- chap4_pet.php
+++ chap4_pet.php
@@ -1,12 +1,12 @@
 <?php
 function displayMsg($owner) {
-    $animal_types = array($owner["animal"]);
+    $animal_types = array_unique($owner["animal"]);
     $animal = implode(",", $animal_types);
     $count = count($owner["animal"]);
     $msg = <<< EOS
 {$owner["name"]}さんは
 {$animal}を
-{$count}匹飼っています。
+{$count}匹飼っています。\n
 EOS;
     echo $msg; 
 }
