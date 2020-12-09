26bf49c00eb200dcdbbf29aace9010637818114f
ishidukaryo <hekkusyun1@gmail.com>
2020-12-10 02:18:59 +0900

    chapter4課題

--- chap4_pet.php
+++ chap4_pet.php
@@ -1,13 +1,8 @@
 <?php
-function displayMsg($owner, $animal, $count) {
-    function array_unique($owner) {
-        $animal_types = array($owner["animal"]);
-        $animal = implode(",", $animal_types);
-    }
-    function count($owner) {
-        $count = count($owner["animal"]);
-        return $count;
-    }
+function displayMsg($owner) {
+    $animal_types = array($owner["animal"]);
+    $animal = implode(",", $animal_types);
+    $count = count($owner["animal"]);
     $msg = <<< EOS
 {$owner["name"]}さんは
 {$animal}を
