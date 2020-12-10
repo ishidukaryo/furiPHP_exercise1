2d648f191cd7624d4d1db49bdce058b0ab2054b7
ishidukaryo <hekkusyun1@gmail.com>
2020-12-10 13:30:04 +0900

    chapter4課題

--- chap4_pet.php
+++ chap4_pet.php
@@ -3,10 +3,10 @@ function displayMsg($owner) {
     $animal_types = array_unique($owner["animal"]);
     $animal = implode(",", $animal_types);
     $count = count($owner["animal"]);
-    $msg = <<< EOS
+    $msg = <<<EOM
 {$owner["name"]}さんは
 {$animal}を
 {$count}匹飼っています。\n
-EOS;
+EOM;
     echo $msg; 
 }
--- chap4_price.php
+++ chap4_price.php
@@ -1,18 +1,18 @@
 <?php
-function  add($item1, $item2, $item3){
+function add($item1, $item2, $item3){
     $sum = $item1 + $item2 + $item3;
     return $sum;
 }
 function totalTax($sum) {
-    $price = floor( $sum * 1.10);
+    $price = floor($sum * 1.10);
     return $price;
 }
 function displayMsg($name, $sum, $price) {
-    $msg = <<< EOM
+    $msg = <<<EOM
 {$name}様
 ご注文を承りました。
 合計{$sum}円です。
-｛$price}円(税込み)になります。
+{$price}円(税込み)になります。
 EOM;
     echo $msg;
 }
